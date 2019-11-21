813985d598ff25381ac13352108ca0afea23c626
Brian Rickard <brickard15@gmail.com>
2019-11-21 11:37:56 -0600

    Revert "Merge branch 'master' into feature-3"
    This reverts commit cd0880ea6b0372403c780ed1994b746c6d941b12.

--- src/App.java
+++ src/App.java
@@ -2,18 +2,10 @@ class App
 { 
     public static void main(String args[]) 
     { 
-        BeforeStart();
         System.out.println("Hello, World"); 
     } 
-    
-    public static void BeforeStart(){
-        System.out.println("Before Start");
-    }
-<<<<<<< HEAD
-    
+
     public static void GoodBye(){
         System.out.println("Goodbye, World");
     }
-=======
->>>>>>> parent of cd0880e... Merge branch 'master' into feature-3
 } 
