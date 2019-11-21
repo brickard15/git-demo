f633e893fa50c261742b5635c4c3c36ff7b2ff2e
Brian Rickard <brickard15@users.noreply.github.com>
2019-11-21 10:33:50 -0600

    Added feature 2

--- src/App.java
+++ src/App.java
@@ -3,5 +3,9 @@ class App
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
-    } 
+    }
+    
+    public static void GoodBye(){
+        System.out.println("Goodbye, World");
+    }
 } 
