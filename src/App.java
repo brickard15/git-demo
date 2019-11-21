f58befef939ec78778c7fc541f712b07c53b8aa7
Brian Rickard <brickard15@users.noreply.github.com>
2019-11-21 10:32:50 -0600

    Updated App

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 class App 
 { 
-    public static void main(String args[]) 
-    { 
+    public static void main(String args[]) { 
         System.out.println("Hello, World"); 
+        GoodBye();
     } 
+    
+    public static void GoodBye(){
+        System.out.println("Goodbye");
+    }
 } 
